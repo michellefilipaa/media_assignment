40052ac05bc6e007a8d1dace1a13dceec487c424
ashademeij <a.demeij@student.maastrichtuniversity.nl>
2024-03-23 21:11:30 +0100

    fairnessss

--- values/FinalRecommender.py
+++ values/FinalRecommender.py
@@ -54,9 +54,77 @@ class FinalRecommender:
         return self.cas_instance.make_recommendations(self.genre)
 
     def fairness(self, recommendations):
-        # input code to see if the recommendations made are indeed representative
-        # if not, regenerate the recommendations
-        pass
+        top10 = recommendations.head(10)  # Select the first 10 entries
+        total_recommendations = len(top10)
+
+
+        nonMinority_ratio = top10['representation'].eq(1).sum() / total_recommendations
+        minority_ratio = top10['representation'].eq(2).sum() / total_recommendations
+        # Na items are mixed sport teams and other mixed stuff (cartoons)
+        mixed_ratio = (top10['representation'].eq(3).sum() + top10['representation'].isna().sum()) / total_recommendations
+
+        #print(nonMinority_ratio, minority_ratio, mixed_ratio)
+
+        # target distribution for each category (majority are mixed that way 2/4 and others 1/4)
+        target_distribution = np.array([1/4, 1/4, 2/4])
+
+        # Intermediate fairness scores
+        intermediate_scores = np.abs(np.array([nonMinority_ratio, minority_ratio, mixed_ratio]) - target_distribution)
+        print(intermediate_scores)
+
+        # Fairness score of the current top 10 recommendation
+        overall_fairness_score = 1 - np.max(intermediate_scores)
+        print(overall_fairness_score)
+
+        
+        
+        
+        # Check if fairness score is less than 0.5, if not make a better/more fair recommendation
+        if overall_fairness_score < 0.5:
+            found_better = False
+            
+            # Find 'group' that is most represented
+            highest_representation = 0
+
+            if nonMinority_ratio > minority_ratio and nonMinority_ratio > mixed_ratio:
+                highest_representation = 0
+            elif minority_ratio > nonMinority_ratio and minority_ratio > mixed_ratio:
+                highest_representation = 1
+            else:
+                highest_representation = 2
+
+
+            # Find the index of a row with a movie with overrepresented group, this will be replaced
+            index_to_replace = top10[top10['representation'] == highest_representation].index[0]
+
+            # Look for other recommendations that improve the score
+            for i in range(total_recommendations, len(recommendations)):
+                next_entry = recommendations.iloc[i]
+
+                # Add the next entry to df and recalculate fairness score
+                new_df = pd.concat([top10.drop(index_to_replace), next_entry.to_frame().T], ignore_index=True)
+                total_recommendations = len(new_df)
+
+                nonMinority_ratio = new_df['representation'].eq(1).sum() / total_recommendations
+                minority_ratio = new_df['representation'].eq(2).sum() / total_recommendations
+                mixed_ratio = (new_df['representation'].eq(3).sum() + new_df['representation'].isna().sum()) / total_recommendations
+
+                new_intermediate_scores = np.abs(np.array([nonMinority_ratio, minority_ratio, mixed_ratio]) - target_distribution)
+                new_overall_fairness_score = 1 - np.max(new_intermediate_scores)
+
+                # If the score is greater than or equal to 0.5, update df and return the "fairer" recommendation
+                if new_overall_fairness_score >= 0.5:
+                    overall_fairness_score = new_overall_fairness_score
+                    top10 = new_df
+                    found_better = True
+                    break
+
+                # If no better recommendation is found, return the "unfair" recommendation
+                if not found_better:
+                    print("no better found")
+
+        print("Final overall fairness score:", overall_fairness_score)
+        return top10
     
     def filter_polarity_and_genre(self, df):
         if self.genre != 'Any':
